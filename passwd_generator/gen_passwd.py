bcaeb066a024bef61c5638aef98e7a06260a3e7c
vvimjam <akhilesh.vvs@gmail.com>
2020-03-29 04:24:26 +0100

    Added ability to clear out clipboard after set duration

--- passwd_generator/gen_passwd.py
+++ passwd_generator/gen_passwd.py
@@ -5,21 +5,61 @@ import re
 import secrets
 import string
 import pyperclip
+import time
+import os
 
-len_passwd = random.randint(16, 32)
-alphabet = string.ascii_letters
-numeric = string.digits
-symbols =string.punctuation
+def func_clear_console():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
 
-while True:
-    password = ''.join(secrets.choice(alphabet + numeric + symbols) for i in range(len_passwd))
-    if (any(c.islower() for c in password)
-            and any(c.isupper() for c in password)
-            and sum(c.isdigit() for c in password) >= 3):
-        break
 
-# print (password)
-pyperclip.copy(password)
-result = pyperclip.paste()
+def func_gen_password():
+    len_passwd = random.randint(16, 32)
+    alphabet = string.ascii_letters
+    numeric = string.digits
+    symbols =string.punctuation
 
-print ('copied password to clipboard!')
+    while True:
+        password = ''.join(secrets.choice(alphabet + numeric + symbols) for i in range(len_passwd))
+        if (any(c.islower() for c in password)
+                and any(c.isupper() for c in password)
+                and sum(c.isdigit() for c in password) >= 3):
+            break
+
+    return password
+
+
+def func_countdown(delay):
+    # Loops thru & prints time left to clear clipboard
+    sleep_time_in_sec = delay
+    while True:
+        print ('password copied to clipboard!')
+        print("Password will be cleared out in", sleep_time_in_sec)
+        time.sleep(1)
+        sleep_time_in_sec = (sleep_time_in_sec - 1)
+        func_clear_console()
+        if sleep_time_in_sec == 0:
+            break  
+
+
+def func_clip_clear_payload(generated_password):
+    # Clip password
+    pyperclip.copy(generated_password)
+
+    func_countdown(15)
+
+    # Clear clipboard
+    pyperclip.copy("")
+    print ('Password cleared from clipboard!')
+
+
+# clear console
+func_clear_console()
+
+# generate password
+gen_password = func_gen_password()
+
+# clip n clear password
+func_clip_clear_payload(gen_password)
